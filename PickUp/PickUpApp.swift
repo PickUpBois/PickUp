8fd13415668b0e3026a1663a5311b057aaa614be
Arian Rahbar <arahbar@usc.edu>
2021-04-04 00:43:23 -0500

    Initial Commit

---
+++ PickUp/PickUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickUpApp.swift
+//  PickUp
+//
+//  Created by Arian Rahbar on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct PickUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
