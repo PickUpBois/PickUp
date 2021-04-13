eca20356d1fa982c8fc99515d87b17955a209406
Arian Rahbar <arahbar@usc.edu>
2021-04-13 14:45:08 -0500

    tab stuff

---
+++ PickUp/ui/TabbarStuff/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Tabbar
+//
+//  Created by David Reynolds on 4/12/21.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
