e684dccdd8ebff814400b3f7133151acd5ad5c74
Ashwin <shwinster101@gmail.com>
2021-04-23 18:40:18 -0500

    wed 4/20

--- PickUp/ui/auth/ProfileView.swift
+++ PickUp/ui/auth/ProfileView.swift
@@ -8,66 +8,167 @@
 import SwiftUI
 import Combine
 
+/**
+ Code to go inside side menu to say what each option is
+ */
+//struct MenuContent: View {
+//    var body: some View {
+//        List {
+//            Text("My Profile").onTapGesture {
+//                print("My Profile")
+//            }
+//            Text("Posts").onTapGesture {
+//                print("Posts")
+//            }
+//            Text("Logout").onTapGesture {
+//                print("Logout")
+//            }
+//        }
+//    }
+//}
+
+/**
+ SideMenu for setting
+ */
+//struct SideMenu: View {
+//    let width: CGFloat
+//    let isOpen: Bool
+//    let menuClose: () -> Void
+//
+//    var body: some View {
+//        ZStack {
+//            GeometryReader { _ in
+//                EmptyView()
+//            }
+//            .background(Color.gray.opacity(0.3))
+//            .opacity(self.isOpen ? 1.0 : 0.0)
+//            .animation(Animation.easeIn.delay(0.25))
+//            .onTapGesture {
+//                self.menuClose()
+//            }
+//
+//            HStack {
+//                MenuContent()
+//                    .frame(width: self.width)
+//                    .background(Color.white)
+//                    .offset(x: self.isOpen ? 0 : -self.width)
+//                    .animation(.default)
+//
+//                Spacer()
+//            }
+//        }
+//    }
+//}
+
 struct ProfileView: View {
     @ObservedObject var viewModel: ViewModel
+    @State private var selection = 1
+    @State var menuOpen: Bool = false
     
     init(viewModel: ViewModel = ViewModel()) {
         self.viewModel = viewModel
     }
     //Navigation bar
     var body: some View {
-        ScrollView {
-        // Stacks everything on page
-        VStack(alignment: .center) {
-            Text("Jim's Profile").font(.title3) // Leading title on page
-                .fontWeight(.bold)
-                .padding(.top)
-        Spacer().frame(minHeight: 10, maxHeight: 20) // Space between profile picture and leading title
+        NavigationView{
             
-            // Stacks for profile picture
-            HStack {
-                VStack{
-                Image("Jim").frame(width: 100, height: 100, alignment: .center)
-                .clipShape(Circle())
-                .shadow(radius: 2)
-                .overlay(Circle().stroke(Color.black, lineWidth: 5))
-                    .padding(.trailing, 20)
-                    Text("Jim Heise").font(.headline).fontWeight(.bold).foregroundColor(Color.black)
-                    Text("Cyclones").font(.headline).fontWeight(.light).foregroundColor(Color.red)
-                Spacer()//Space between profile picture center of page
-            }
-                VStack {
-                Text("Total").font(.headline).foregroundColor(Color.black)
-                Text("Pickups").font(.headline).foregroundColor(Color.black)
-                    Text("50").font(.title).fontWeight(.bold)
-                    Spacer()
-                }
-                VStack {
-                Text("Team").font(.headline).foregroundColor(Color.black)
-                Text("Members").font(.headline).foregroundColor(Color.black)
-                    Text("50").font(.title).fontWeight(.bold)
-                    Spacer()
-                }
-                VStack {
-                Text("GOAT").font(.headline).foregroundColor(Color.black)
-                Text("Meter").font(.headline).foregroundColor(Color.black)
-                Image(systemName: "battery.25").font(.system(size: 40))
-                    .foregroundColor(.green)
-                    Spacer()
-                }
-            }
             
-        Spacer().frame(minHeight: 140, maxHeight: 800)
-        if (viewModel.loading) {
-            Text("Loading!")
-        } else {
-            Button("Logout", action: {() in
-                self.viewModel.logout()
-                })
+            ScrollView {
+            // Stacks everything on page
+
+                VStack(alignment: .center) {
+                Text("Jim's Profile").font(.title3) // Leading title on page
+                    .fontWeight(.bold)
+                    .padding(.top)
+            Spacer().frame(minHeight: 10, maxHeight: 20) // Space between profile picture and leading title
+                
+                // Stacks for profile picture
+                HStack {
+                    VStack{
+                    Image("Jim").frame(width: 100, height: 100, alignment: .center)
+                    .clipShape(Circle())
+                    .shadow(radius: 2)
+                    .overlay(Circle().stroke(Color.black, lineWidth: 5))
+                        .padding(.trailing, 20)
+                        Text("Jim Heise").font(.headline).fontWeight(.bold).foregroundColor(Color.black)
+                        Text("Cyclones").font(.headline).fontWeight(.light).foregroundColor(Color.red)
+                    Spacer()//Space between profile picture center of page
+                        }
+                    VStack {
+                    Text("Total").font(.headline).foregroundColor(Color.black)
+                    Text("Pickups").font(.headline).foregroundColor(Color.black)
+                        Text("50").font(.title).fontWeight(.bold)
+                        Spacer()
+                    }
+                    VStack {
+                    Text("Team").font(.headline).foregroundColor(Color.black)
+                    Text("Members").font(.headline).foregroundColor(Color.black)
+                        Text("50").font(.title).fontWeight(.bold)
+                        Spacer()
+                    }
+                    VStack {
+                    Text("GOAT").font(.headline).foregroundColor(Color.black)
+                    Text("Meter").font(.headline).foregroundColor(Color.black)
+                    Image(systemName: "battery.25").font(.system(size: 40))
+                        .foregroundColor(.green)
+                        Spacer()
+                    }
+                }
+                
+            
+                Picker("", selection: $selection) {
+                    Image(systemName: "circle.grid.2x2.fill").tag(0)
+                    Image(systemName: "person.circle").tag(1)
+                }.pickerStyle(SegmentedPickerStyle()).padding(.horizontal)
+                
+                
+                Spacer().frame(minHeight: 140, maxHeight: 800)
+                if (viewModel.loading) {
+                    Text("Loading!")
+                } else {
+                    Button("Logout", action: {() in
+                        self.viewModel.logout()
+                        })
+                    }
+                }
+
+            
+                
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        Text("Profile").font(.headline)
+//                        if !self.menuOpen {
+//                            Button(action: {
+//                                self.openMenu()
+//                            }, label: {
+//                                Text("Open")
+//                            })
+                        NavigationLink(destination: SignUpView()) {
+                            Image(systemName: "gearshape.fill")
+                            }
+//                        }
+                        
+                    }
+                    //calls the side menu
+//                    SideMenu(width:270,
+//                             isOpen: self.menuOpen,
+//                             menuClose: self.openMenu)
+                    
+                }
             }
         }
+        
+            
         }
-    }
+
+
+    // function to open the menu
+//    func openMenu() {
+//        self.menuOpen.toggle()
+//    }
 }
 extension ProfileView {
     class ViewModel: ObservableObject {
