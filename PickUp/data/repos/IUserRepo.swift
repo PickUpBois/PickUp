0b59ed34a1df0c6f2edaf0ccbd62840f7ce8c789
Arian Rahbar <arahbar@usc.edu>
2021-04-27 03:04:55 -0500

    some stuff

---
+++ PickUp/data/repos/IUserRepo.swift
@@ -0,0 +1,18 @@
+//
+//  IUserRepo.swift
+//  PickUp
+//
+//  Created by Arian Rahbar on 4/18/21.
+//
+
+import Foundation
+import Combine
+import Firebase
+
+protocol IUserRepo {
+    func create(item: User) -> AnyPublisher<Void, Error>
+    func get(id: String) -> AnyPublisher<User, Error>
+    func update(id: String, fields: [ AnyHashable : Any ]) -> AnyPublisher<Void, Error>
+    func delete(id: String) -> AnyPublisher<Void, Error>
+    func observe(id: String) -> AnyPublisher<User, Error>
+}
