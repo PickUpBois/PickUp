3ef5f0e2e1dddda1bd9e2d449216a130d40dfe50
Ashwin <shwinster101@gmail.com>
2021-05-19 16:45:32 -0500

    transparent popup, cleaned up popup view, added start/finish pickup buttons

--- PickUp/ui/auth/HomeScreen/PopupView.swift
+++ PickUp/ui/auth/HomeScreen/PopupView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct PopupView: View {
+    @State var showPopUp = false
     var body: some View {
-        ScrollView{
-HStack(alignment: .top){
+        
+            Spacer().frame(height: 300)
+            HStack{
             VStack{
-                Text("self.viewModel.event.name")
+                Text("Singles") //self.viewModel.event.name
     .fontWeight(.heavy)
 Spacer()
-Text("self.viewModel.event.info")
+Text("2 out of 3 sets") //self.viewModel.event.info
 Spacer()
 //join event 'button'
 Text("+ Join")
@@ -25,36 +27,55 @@ Text("+ Join")
                   }
         Spacer()
             VStack{
+                
                 HStack{
                 Image(systemName:"location.fill")
                     Text("Location")}
-            Spacer()
+            Spacer().frame(height: 5)
                 HStack{
                 Image(systemName:"calendar")
-                    Text("")}
-            Spacer()
+                    Text("Date")}
+            Spacer().frame(height: 5)
                 HStack{
                 Image(systemName:"clock.fill")
-                    Text("")}
-            Spacer()
+                    Text("Time")}
+            Spacer().frame(height: 5)
                 HStack{
                 Image(systemName:"person.3.fill")
                     Text("people")}
-            Spacer()
-                Text("Invite")
-                    .fontWeight(.heavy)
-                    .padding(.all, 5.0)
-                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+            Spacer().frame(height: 5)
+                    Text("Invite")
+                        .fontWeight(.heavy)
+                        .padding(.all, 5.0)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                
               }
                   }
-        .padding(.all, 25.0)
-        .background(Color(red: 1, green: 0.5, blue: 0, opacity: 0.2))
+        .padding(.all, 30.0)
+            .background(Color(red: 1, green: 0.7, blue: 0, opacity: 0.9))
         .cornerRadius(8)
         .padding(.horizontal, 20)
             
+        HStack{
+            
+        Button(action: {},
+               label: {Text("Start Pickup")
+                .padding(.all, 5.0)
+                .font(.body)
+                .foregroundColor(Color.white)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
+               })
+        
+            Button(action: {FinishPickupView()},
+               label: {Text("Finish Pickup")
+                .padding(.all, 5.0)
+                .font(.body)
+                .foregroundColor(Color.white)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
+               })
+        }
         
         }
-}
 
 }
 
--- PickUp/ui/auth/HomeScreen/UpcomingPickUps/PickUpView.swift
+++ PickUp/ui/auth/HomeScreen/UpcomingPickUps/PickUpView.swift
@@ -8,33 +8,80 @@
 import SwiftUI
 
 struct PickUpView: View {
+    @State var showPopUp = false
     var body: some View {
-        VStack {
-            HStack{
-                Text("Singles")
-                    .fontWeight(.heavy)
-                    .multilineTextAlignment(.center)
-                    .lineLimit(1)
-                Image(systemName:"calendar")
-                    .foregroundColor(Color.red)
-                Text("Date")
-                        Spacer()
-                        .frame(minWidth: 10, maxWidth: 10)
-                        .lineLimit(1)
-            }
-                Spacer().frame(height: 2)
-                HStack(alignment: .center){
-                    Image(systemName:"location.fill")
-                        .foregroundColor(Color.blue)
-                        .lineLimit(1)
-                    Text("Location")
-                    Text("Going")
-                        .foregroundColor(Color.green)
-                        .lineLimit(1)
-            }
+       
+        HStack(alignment: .center) {
+            Button(action: {
+                self.showPopUp.toggle()
+            }, label: {
 
-        }
+                VStack {
+                    HStack{
+                        Text("Singles")
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.black)
+                            .multilineTextAlignment(.center)
+                            .lineLimit(1)
+                        Image(systemName:"calendar")
+                            .foregroundColor(Color.red)
+                        Text("Date")
+                            .foregroundColor(Color.black)
+                                Spacer()
+                                .frame(minWidth: 10, maxWidth: 10)
+                                .lineLimit(1)
+                    }
+                        Spacer().frame(height: 2)
+                        HStack(alignment: .center){
+                            Image(systemName:"location.fill")
+                                .foregroundColor(Color.blue)
+                                .lineLimit(1)
+                            Text("Location")
+                                .foregroundColor(Color.blue)
+                            Text("Going")
+                                .foregroundColor(Color.green)
+                                .lineLimit(1)
+                    }
+
+                }
+                
+                
+            })
+        }.sheet(isPresented: $showPopUp, content: {
+           
+            PopupView()
+            
+            Button(action: {
+                self.showPopUp.toggle()
+            },label: {
+                Text("Dismiss")
+                    .foregroundColor(Color.white)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 10)
+                    .background(Color.black.opacity(0.8))
+                    .cornerRadius(9)
+                    .padding(.horizontal, 20)
+            })
+            
+            Spacer().frame(height: 300)
+            .background(BackgroundClearView())
+            
+        })
+        
+        
     }
+}
+
+struct BackgroundClearView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
 }
 
 struct PickUpView_Previews: PreviewProvider {
