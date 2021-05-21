03ad9d6253cfb44df11f6491d3a4123c1f1178ed
Arian Rahbar <arahbar@usc.edu>
2021-05-21 14:47:03 -0500

    fixed bug

--- PickUp/ui/auth/SearchView.swift
+++ PickUp/ui/auth/SearchView.swift
@@ -5,41 +5,41 @@
 //  Created by Arian Rahbar on 5/21/21.
 //
 
-import SwiftUI
-import UIKit
-struct SearchView: UIViewRepresentable {
-    @Binding var text: String
-    
-    class Coordinator: NSObject, UISearchBarDelegate {
-        @Binding var text: String
-        
-        init(text: Binding<String>) {
-            _text = text
-        }
-        
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            text = searchText
-        }
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(text: $text)
-    }
-    
-    func makeUIView(context: UIViewRepresentableContext<SearchView>) -> some UISearchBar {
-        let searchBar = UISearchBar(frame: .zero)
-        searchBar.delegate = context.coordinator
-        searchBar.searchBarStyle = .minimal
-        return searchBar
-    }
-    
-    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchView>) {
-        uiView.text = text
-    }
-}
-
-struct SearchView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchView(text: .constant(""))
-    }
-}
+//import SwiftUI
+//import UIKit
+//struct SearchView: UIViewRepresentable {
+//    @Binding var text: String
+//    
+//    class Coordinator: NSObject, UISearchBarDelegate {
+//        @Binding var text: String
+//
+//        init(text: Binding<String>) {
+//            _text = text
+//        }
+//
+//        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//            text = searchText
+//        }
+//    }
+//
+//    func makeCoordinator() -> Coordinator {
+//        return Coordinator(text: $text)
+//    }
+//
+//    func makeUIView(context: UIViewRepresentableContext<SearchView>) -> some UISearchBar {
+//        let searchBar = UISearchBar(frame: .zero)
+//        searchBar.delegate = context.coordinator
+//        searchBar.searchBarStyle = .minimal
+//        return searchBar
+//    }
+//
+//    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchView>) {
+//        uiView.text = text
+//    }
+//}
+//
+//struct SearchView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SearchView(text: .constant(""))
+//    }
+//}
