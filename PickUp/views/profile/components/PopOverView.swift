62487edf2071a23d07ba929011cb54a4dca027e7
David Reynolds <dreynolds1216@gmail.com>
2021-06-15 12:14:06 -0500

    popover/profile

---
+++ PickUp/views/profile/components/PopOverView.swift
@@ -0,0 +1,44 @@
+//
+//  SwiftUIView.swift
+//  PickUp
+//
+//  Created by David Reynolds on 6/14/21.
+//
+
+import SwiftUI
+
+struct PopOverView: View {
+    var body: some View {
+        
+        VStack(alignment: .center, spacing: 18) {
+            
+            Button(action:{
+            }) {
+                    Text("Remove Friend")
+            }
+            
+            Divider()
+            Button(action:{
+            }) {
+                    Text("Block")
+            }
+            
+            Divider()
+            Button(action:{
+                }) {
+                    Text("Report")
+            }
+            
+        }
+        .foregroundColor(.black)
+        .frame(width: 150)
+        .padding()
+        .background(Color.white.opacity(0.2))
+}
+}
+
+struct PopOverView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopOverView()
+    }
+}
