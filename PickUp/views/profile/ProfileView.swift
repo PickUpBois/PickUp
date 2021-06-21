35d08ee4d78ee844039b5fd965b7514620f4ecf6
Arian Rahbar <arahbar@usc.edu>
2021-06-21 11:47:16 -0500

    removed loading

--- PickUp/views/profile/ProfileView.swift
+++ PickUp/views/profile/ProfileView.swift
@@ -47,90 +47,86 @@ struct ProfileView: View {
     
     var body: some View {
         let showAlert: Binding<Bool> = .init(get: {self.alertType != nil}, set: {_ in })
-        if viewModel.loading {
-                ProgressView()
-            } else {
-                VStack {
-                // Stacks everything on page
-                    // Stacks for profile picture
-                    ProfileHeaderView(auth: auth, alertType: $alertType).environmentObject(self.viewModel)
-                        .frame(height: 200)
+            VStack {
+            // Stacks everything on page
+                // Stacks for profile picture
+                ProfileHeaderView(auth: auth, alertType: $alertType).environmentObject(self.viewModel)
+                    .frame(height: 200)
 
 
-                    Picker("", selection: $selection) {
-                        Text("Past Pickups").foregroundColor(Color.blue).tag(1)
-                        Text("Upcoming Pickups").foregroundColor(Color.red).tag(0)
-                    }.pickerStyle(SegmentedPickerStyle()).padding(.horizontal)
+                Picker("", selection: $selection) {
+                    Text("Past Pickups").foregroundColor(Color.blue).tag(1)
+                    Text("Upcoming Pickups").foregroundColor(Color.red).tag(0)
+                }.pickerStyle(SegmentedPickerStyle()).padding(.horizontal)
 
-                   
-                    if selection == 1 {
-                        PastPickupView().environmentObject(viewModel)
-                    }
-                    //second picker option
-                    
-                    else {
-                        UpcomingPickupsView().environmentObject(viewModel)
-                    }
-                    
-                    
-                    Spacer().frame(minHeight: 5, maxHeight: 10)
-                    
-                    //popover for unfriending, blocking, and reporting
-                        
-                    if popover == true{
+               
+                if selection == 1 {
+                    PastPickupView().environmentObject(viewModel)
+                }
+                //second picker option
                 
-                        PopOverView(alertType: $alertType).environmentObject(self.viewModel)
+                else {
+                    UpcomingPickupsView().environmentObject(viewModel)
+                }
+                
+                
+                Spacer().frame(minHeight: 5, maxHeight: 10)
+                
+                //popover for unfriending, blocking, and reporting
+                    
+                if popover == true{
+            
+                    PopOverView(alertType: $alertType).environmentObject(self.viewModel)
+                    
+                }
+            }
+            .alert(isPresented: showAlert) {
+                switch alertType! {
+                case .addFriend:
+                    return self.addFriendAlert
+                case .removeFriend:
+                    return self.removeFriendAlert
+                }
+            }
+            .onAppear { () in
+                print("retrieving profile state")
+                viewModel.retrieveState()
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarLeading) {
+                    HStack {}
+                }
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        Image("logo2")
+                            .resizable()
+                            .padding(.top, -15)
+                            .frame(width: 125, height: 30)
+                            .scaledToFit()
+                            
+                        
+                        if auth {
+                            Spacer().frame(minWidth: 100, idealWidth: 100, maxWidth: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            NavigationLink(destination: ProfileSettingsView().environmentObject(self.viewModel)) {
+                                Image(systemName: "gearshape.fill")
+                            }
+                        }
+                        
+                        else{
+                            Spacer().frame(minWidth: 120, idealWidth: 120, maxWidth: 100, alignment: .center)
+                            Button( action: {
+                                withAnimation(.easeIn){popover.toggle()}
+
+                            }, label: {
+                                Image(systemName: "list.dash")
+                                    .padding(.trailing)
+                            })
+                        }
+
                         
                     }
-                }
-                .alert(isPresented: showAlert) {
-                    switch alertType! {
-                    case .addFriend:
-                        return self.addFriendAlert
-                    case .removeFriend:
-                        return self.removeFriendAlert
-                    }
-                }
-                .onAppear { () in
-                    print("retrieving profile state")
-                    viewModel.retrieveState()
-                }
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar{
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        HStack {}
-                    }
-                    ToolbarItem(placement: .principal) {
-                        HStack {
-                            Image("logo2")
-                                .resizable()
-                                .padding(.top, -15)
-                                .frame(width: 125, height: 30)
-                                .scaledToFit()
-                                
-                            
-                            if auth {
-                                Spacer().frame(minWidth: 100, idealWidth: 100, maxWidth: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                NavigationLink(destination: ProfileSettingsView().environmentObject(self.viewModel)) {
-                                    Image(systemName: "gearshape.fill")
-                                }
-                            }
-                            
-                            else{
-                                Spacer().frame(minWidth: 120, idealWidth: 120, maxWidth: 100, alignment: .center)
-                                Button( action: {
-                                    withAnimation(.easeIn){popover.toggle()}
-
-                                }, label: {
-                                    Image(systemName: "list.dash")
-                                        .padding(.trailing)
-                                })
-                            }
-
-                            
-                        }
-                        .padding(.leading, 110)
-                    }
+                    .padding(.leading, 110)
                 }
             }
         
