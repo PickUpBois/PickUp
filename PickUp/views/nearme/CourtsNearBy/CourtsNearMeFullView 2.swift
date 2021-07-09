156f4b03656d780fd2d24981c4059f7726f4684e
David Reynolds <dreynolds1216@gmail.com>
2021-07-08 23:12:06 -0500

    k

---
+++ PickUp/views/nearme/CourtsNearBy/CourtsNearMeFullView 2.swift
@@ -0,0 +1,20 @@
+//
+//  CourtsNearMeFullView.swift
+//  PickUp
+//
+//  Created by David Reynolds on 6/8/21.
+//
+
+import SwiftUI
+
+struct CourtsNearMeFullView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourtsNearMeFullView_Previews: PreviewProvider {
+    static var previews: some View {
+        CourtsNearMeFullView()
+    }
+}
